ab4f37c80a01b6359fc608199721056665a48f4d
Daniel Montet <dmontetproff@gmail.com>
2019-02-15 12:53:58 +0300

    Add event listeners for different buttons to capture submission of number

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+document.getElementById('num1').addEventListener("click",function(){
+document.getElementById("var").innerHTML = "Hello World";
+});
+document.getElementById('num2').addEventListener("click",function(){
+document.getElementById("var").innerHTML = "Hello World2";
+});
+document.getElementById('num3').addEventListener("click",function(){
+document.getElementById("var").innerHTML = "Hello World3";
+});
+
+if (a===b && a===c && b===c){
+alert(equilateral);
+}
+else if ((a+b)<=c || (a+c)<=b || (b+c)<=a){
+alert("cannot form triangle");
+}
+else
+}
+do {
+if((a+b)>c || (a+c)>b || (b+c)>a)
+  alert("scalene")
+  break;
+}
+
+while (a!=b && b!=c && a!=c) {
+}
+
+function(){
+  var value=document.getElementById('first_number');
+  var value=document.getElementById('first_number');
+  var value=document.getElementById('third_number');
+}
+
+
+//ui logic
